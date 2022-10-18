94bfc1229657be9e1f50f8536caf25758888ba40
phamphuc2005 <minhpham2001bk@gmail.com>
2022-10-18 22:14:00 +0700

    show users ui

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import {getAllUsers} from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if(response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
-        );
+            <div className="users-container">
+                <div className='title text-center'>Manage users</div>
+                <div className='users-table mt-4 mx-4'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Address</th>
+                            <th>Phonenumber</th>
+                            <th>Actions</th>
+                        </tr>
+                        
+                        {
+                            arrUsers && arrUsers.map((item, index) => {
+                                return(
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>{item.phonenumber}</td>
+                                        <td className='text-center'>
+                                            <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                            <button className='btn-delete'><i className="fas fa-trash-alt"></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }                 
+                    </table>
+                </div>
+            </div>
+        )
     }
-
 }
 
 const mapStateToProps = state => {
